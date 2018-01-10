8c9f2831b67354c0086d8f4c27252c9210e6b0f6
Jotony Guerra <jotonyguerra@gmail.com>
2018-01-10 14:24:09 -0800

    rudimentary tests passing

--- spec/features/signed_in_user_imports_data_spec.rb
+++ spec/features/signed_in_user_imports_data_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 feature 'signed in user imports dexcom data' do
-  context "Normal User" do
+  context "Signed in User" do
    let(:user) {FactoryBot.create(:user)}
    before { login_as(user, scope: :user)}
 
    scenario "sees a 'import dexcom data' link" do
      visit root_path
      expect(page).to have_content("Import Dexcom Data")
+   end
+   scenario "user attempts to upload improper file" do
+     visit root_path
+     click_link("Import Dexcom Data")
+     save_and_open_page
+     click_button('Import Dexcom Data')
+     expect(page).to have_content("please select a valid file")
    end
  end
 end
