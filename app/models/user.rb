674bc69176ce2646f7d4e39563225db62f5ede04
Jotony Guerra <jotonyguerra@gmail.com>
2018-01-23 12:13:06 -0800

    added some landing page info, added model class for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :readings
+  has_many :readings, :comments
 end
